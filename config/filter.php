00024f7545b39dc2cf89494fa12662b8f196e0e6
wuque <91714264+huangbule2024@users.noreply.github.com>
2024-03-15 10:59:18 +0800

    Update filter.php
    注释掉demo

--- config/filter.php
+++ config/filter.php
@@ -4,9 +4,10 @@ return [
     'default' => '$like',
 
     'rule' => [
-        'uuid' => 'uuid|$eq',
-        'created_at' => 'HalfOpenDate|$halfOpen',
-        'department_name' => '$like',
-        '#department_name' => '#department|$like'
+        //demo
+        //'uuid' => 'uuid|$eq',
+        //'created_at' => 'HalfOpenDate|$halfOpen',
+        //'department_name' => '$like',
+        //'#department_name' => '#department|$like'
     ]
 ];
